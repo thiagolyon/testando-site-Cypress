10aeda91d2fde6a7a3e67bb8654152487dee0b1c
Thiago Lyon <thiago.lyon.passos@gmail.com>
2022-06-09 20:42:57 -0300

    Adicionando testes automatizados com Cypress

--- cypress/integration/ongs.spec.js
+++ cypress/integration/ongs.spec.js
@@ -29,9 +29,46 @@ describe('Ongs', () => {
     });
 
     it('devem poder realizar o login no sistema', () => {
-
         cy.visit('http://localhost:3000/');
-        cy.get('input').type(Cypress.env('createdOngId'));
-        cy.get('.button').click();
+        cy.get('[data-cy=id]').type(Cypress.env('createdOngId'));
+        cy.get('[data-cy=button-login]').click();
     });
+
+    it('devem poder fazer logout', () => {
+        cy.login();
+        cy.get('[data-cy=button-logout]').click();
+    });
+
+    it('devem poder cadastrar novos casos', () => {
+        cy.login();
+
+        cy.get('[data-cy="button-new"]').click();
+        cy.get('[data-cy=case-title]').type('Pet para adoção');
+        cy.get('[data-cy="description"]').type('Pet vacinado e castrado');
+        cy.get('[data-cy="value"]').type('0');
+
+        cy.route('POST', '**/incidents').as('newIncident');
+
+        cy.get('[data-cy="register"]').click();
+
+        cy.wait('@newIncident').then((xhr) => {
+            expect(xhr.status).to.eq(200);
+            expect(xhr.response.body).has.property('id');
+            expect(xhr.response.body.id).is.not.null;
+        })
+    } )
+
+    it('Devem poder excluir um caso', () => {
+        cy.createNewIncident();
+        cy.login();
+
+        cy.route('DELETE', '**/incidents/*').as('deleteIncident');
+
+        cy.get('[data-cy="button-delete"]').click();
+
+        cy.wait('@deleteIncident').then((xhr) => {
+            expect(xhr.status).to.eq(204);
+            expect(xhr.response.body).to.be.empty;
+        })
+    })
 });
